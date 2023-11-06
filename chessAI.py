b95fec00f730cd10b992dfddbe046471c81967df
Elias <dv19enn@cs.umu.se>
2023-11-06 12:40:03 +0100

    New eval function using placing of pieces heuristic

--- chessAI.py
+++ chessAI.py
@@ -19,8 +19,8 @@ class MainWindow(QWidget):
         self.AIStart = AIStart
         self.widgetSvg = QSvgWidget(parent=self)
         self.widgetSvg.setGeometry(0, 0, self.boardSize, self.boardSize)
-        # self.chessboard = chess.Board()
-        self.chessboard = chess.Board(fen="7k/P7/4K3/8/8/8/8/8 w - - 0 1")
+        self.chessboard = chess.Board()
+        # self.chessboard = chess.Board(fen="7k/P7/4K3/8/8/8/8/8 w - - 0 1")
 
         self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
         self.widgetSvg.load(self.chessboardSvg)
@@ -53,28 +53,28 @@ class MainWindow(QWidget):
 
 def AIMove(chessboard: chess.Board, window: MainWindow):
 
-    max_weight = -math.inf
-    with chess.polyglot.open_reader("performance.bin") as reader:
-        for entry in reader.find_all(window.chessboard):
-            print(entry.move, entry.weight, entry.learn)
-            if entry.weight > max_weight:
-                max_weight = entry.weight
-                move = entry.move
-    if max_weight > -math.inf:
-        makeMove(chess.Move.from_uci(str(move)), window)
-        return
+    # max_weight = -math.inf
+    # with chess.polyglot.open_reader("performance.bin") as reader:
+    #     for entry in reader.find_all(window.chessboard):
+    #         print(entry.move, entry.weight, entry.learn)
+    #         if entry.weight > max_weight:
+    #             max_weight = entry.weight
+    #             move = entry.move
+    # if max_weight > -math.inf:
+    #     makeMove(chess.Move.from_uci(str(move)), window)
+    #     return
 
     print("Thinking")
 
     start = time.perf_counter()
     result = None
     transPositionTable = dict()
-    i = 2
+    i = 1
     while True:
         result = minMax(chessboard, depth=i, prevMove=None, alpha=-
                         1000, beta=1000, transPositionTable=transPositionTable)
-        if (result[0] == None):
-            print("AI did not find move ", chessboard.move_stack)
+        if (result[0] == None and (chess.Board.is_fifty_moves(chessboard) or chess.Board.is_repetition(chessboard) or chess.Board.is_stalemate(chessboard) or chess.Board.is_fivefold_repetition(chessboard))):
+            print("AI did not find any non-losing move", chessboard.move_stack)
             makeMove(chess.Move.from_uci(
                 str(list(chessboard.legal_moves)[0])), window)
             return
@@ -166,16 +166,128 @@ def makeMove(move: chess.Move, window: MainWindow):
     window.repaint()
 
 
+# def evaluationFunction2(board: chess.Board):
+
+#     if chess.Board.is_fifty_moves(board) or chess.Board.is_repetition(board) or chess.Board.is_stalemate(board) or chess.Board.is_fivefold_repetition(board):
+#         if board.turn == chess.BLACK:
+#             return -99
+#         elif board.turn == chess.WHITE:
+#             return 99
+
+#     if len(list(board.legal_moves)) == 0:
+#         if board.turn == chess.BLACK:
+#             return -math.inf
+#         elif board.turn == chess.WHITE:
+#             return math.inf
+
+#     pawnDiff = calcPieceDiff(chess.PAWN, board)
+#     knightDiff = calcPieceDiff(chess.KNIGHT, board) * 3
+#     bishopDiff = calcPieceDiff(chess.BISHOP, board) * 3
+#     rookDiff = calcPieceDiff(chess.ROOK, board) * 5
+#     queenDiff = calcPieceDiff(chess.QUEEN, board) * 9
+#     return pawnDiff + knightDiff + bishopDiff + rookDiff + queenDiff
+
+
+pawn_tableWHITE = [
+    [0,  0,  0,  0,  0,  0,  0,  0],
+    [50, 50, 50, 50, 50, 50, 50, 50],
+    [10, 10, 20, 30, 30, 20, 10, 10],
+    [5,  5, 10, 25, 25, 10,  5,  5],
+    [0,  0,  0, 20, 20,  0,  0,  0],
+    [5, -5, -10,  0,  0, -10, -5,  5],
+    [5, 10, 10, -20, -20, 10, 10,  5],
+    [0,  0,  0,  0,  0,  0,  0,  0]
+]
+
+knight_tableWHITE = [
+    [-50, -40, -30, -30, -30, -30, -40, -50],
+    [-40, -20,  0,  0,  0,  0, -20, -40],
+    [-30,  0, 10, 15, 15, 10,  0, -30],
+    [-30,  5, 15, 20, 20, 15,  5, -30],
+    [-30,  0, 15, 20, 20, 15,  0, -30],
+    [-30,  5, 10, 15, 15, 10,  5, -30],
+    [-40, -20,  0,  5,  5,  0, -20, -40],
+    [-50, -40, -30, -30, -30, -30, -40, -50]
+]
+
+bishop_tableWHITE = [
+    [-20, -10, -10, -10, -10, -10, -10, -20],
+    [-10,  0,  0,  0,  0,  0,  0, -10],
+    [-10,  0,  5, 10, 10,  5,  0, -10],
+    [-10,  5,  5, 10, 10,  5,  5, -10],
+    [-10,  0, 10, 10, 10, 10,  0, -10],
+    [-10, 10, 10, 10, 10, 10, 10, -10],
+    [-10,  5,  0,  0,  0,  0,  5, -10],
+    [-20, -10, -10, -10, -10, -10, -10, -20]
+]
+
+rook_tableWHITE = [
+    [0,  0,  0,  0,  0,  0,  0,  0],
+    [5, 10, 10, 10, 10, 10, 10,  5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [0,  0,  0,  5,  5,  0,  0,  0]
+]
+
+queen_tableWHITE = [
+    [-20, -10, -10, -5, -5, -10, -10, -20],
+    [-10,  0,  0,  0,  0,  0,  0, -10],
+    [-10,  0,  5,  5,  5,  5,  0, -10],
+    [-5,  0,  5,  5,  5,  5,  0, -5],
+    [0,  0,  5,  5,  5,  5,  0, -5],
+    [-10,  5,  5,  5,  5,  5,  0, -10],
+    [-10,  0,  5,  0,  0,  0,  0, -10],
+    [-20, -10, -10, -5, -5, -10, -10, -20]
+]
+
+king_tableWHITE = [
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-20, -30, -30, -40, -40, -30, -30, -20],
+    [-10, -20, -20, -20, -20, -20, -20, -10],
+    [20, 20,  0,  0,  0,  0, 20, 20],
+    [20, 30, 10,  0,  0, 10, 30, 20]
+]
+
+pawn_tableBLACK = []
+knight_tableBLACK = []
+bishop_tableBLACK = []
+rook_tableBLACK = []
+queen_tableBLACK = []
+king_tableBLACK = []
+
+
+def initPieceSquareTables(whiteTable, blackTable):
+    row = 7
+    for i in range(8):
+        newRow = []
+        for col in range(8):
+            newRow.append(whiteTable[row][col])
+        blackTable.append(newRow)
+        row -= 1
+
+
+def printPieceSqTable(table):
+    print(20*"-")
+    for row in table:
+        for col in row:
+            print(f"{'{:3}'.format(col)},", end="")
+        print()
+    print(20*"-")
+
+
 def evaluationFunction(board: chess.Board):
 
     if chess.Board.is_fifty_moves(board) or chess.Board.is_repetition(board) or chess.Board.is_stalemate(board) or chess.Board.is_fivefold_repetition(board):
-        # print("Draw found")
-        # if chess.Board.is_stalemate(board):
-        # print("Stalemate found")
         if board.turn == chess.BLACK:
-            return -99
+            return -math.inf
         elif board.turn == chess.WHITE:
-            return 99
+            return math.inf
 
     if len(list(board.legal_moves)) == 0:
         if board.turn == chess.BLACK:
@@ -183,12 +295,63 @@ def evaluationFunction(board: chess.Board):
         elif board.turn == chess.WHITE:
             return math.inf
 
-    pawnDiff = calcPieceDiff(chess.PAWN, board)
-    knightDiff = calcPieceDiff(chess.KNIGHT, board) * 3
-    bishopDiff = calcPieceDiff(chess.BISHOP, board) * 3
-    rookDiff = calcPieceDiff(chess.ROOK, board) * 5
-    queenDiff = calcPieceDiff(chess.QUEEN, board) * 9
-    return pawnDiff + knightDiff + bishopDiff + rookDiff + queenDiff
+    whiteSum = 0
+    blackSum = 0
+
+    for row in range(8):
+        for col in range(8):
+            numerical = 8 * row + col
+            piece = board.piece_at(numerical)
+            if piece == None:
+                continue
+            if piece.piece_type == chess.PAWN:
+
+                if piece.color == chess.WHITE:
+
+                    whiteSum += 100
+                    whiteSum += pawn_tableWHITE[7-row][col]
+
+                else:
+                    blackSum += 100
+                    blackSum += pawn_tableBLACK[7-row][col]
+
+            elif piece.piece_type == chess.KNIGHT:
+                if piece.color == chess.WHITE:
+                    whiteSum += 320
+                    whiteSum += knight_tableWHITE[7-row][col]
+                else:
+                    blackSum += 320
+                    blackSum += knight_tableBLACK[7-row][col]
+            elif piece.piece_type == chess.BISHOP:
+                if piece.color == chess.WHITE:
+                    whiteSum += 330
+                    whiteSum += bishop_tableWHITE[7-row][col]
+                else:
+                    blackSum += 330
+                    blackSum += bishop_tableBLACK[7-row][col]
+            elif piece.piece_type == chess.ROOK:
+                if piece.color == chess.WHITE:
+                    whiteSum += 500
+                    whiteSum += rook_tableWHITE[7-row][col]
+                else:
+                    blackSum += 500
+                    blackSum += rook_tableBLACK[7-row][col]
+            elif piece.piece_type == chess.QUEEN:
+                if piece.color == chess.WHITE:
+                    whiteSum += 900
+                    whiteSum += queen_tableWHITE[7-row][col]
+                else:
+                    blackSum += 900
+                    blackSum += queen_tableBLACK[7-row][col]
+            elif piece.piece_type == chess.KING:
+                if piece.color == chess.WHITE:
+                    whiteSum += 20000
+                    whiteSum += king_tableWHITE[7-row][col]
+                else:
+                    blackSum += 20000
+                    blackSum += king_tableBLACK[7-row][col]
+    # print(f"whiteSum = {whiteSum} blackSum = {blackSum}")
+    return blackSum - whiteSum
 
 
 def calcPieceDiff(piece: chess.PieceType, board: chess.Board) -> int:
@@ -223,10 +386,7 @@ def sortMoveList(moves: chess.LegalMoveGenerator, board: chess.Board):
 
 
 def minMax(board: chess.Board, depth: int, prevMove: chess.Move, alpha: int, beta: int, transPositionTable: dict):
-    # hashVal = hash(str(board.fen) + str(depth))
-    # if hashVal in transPositionTable and depth != 0 and depth != -1:
-    #     # print("Looking up")
-    #     return minMax(board=board, depth=depth-1, prevMove=transPositionTable[hashVal], alpha=alpha, beta=beta, transPositionTable=transPositionTable)
+
     result = sortMoveList(board.legal_moves, board)
     attackMoves = result[0]
     noAttackMoves = result[1]
@@ -307,6 +467,16 @@ def legalMove(board: chess.Board, move: str):
 
 
 if __name__ == "__main__":
+    initPieceSquareTables(pawn_tableWHITE, pawn_tableBLACK)
+    initPieceSquareTables(knight_tableWHITE, knight_tableBLACK)
+    initPieceSquareTables(bishop_tableWHITE, bishop_tableBLACK)
+    initPieceSquareTables(rook_tableWHITE, rook_tableBLACK)
+    initPieceSquareTables(queen_tableWHITE, queen_tableBLACK)
+    initPieceSquareTables(king_tableWHITE, king_tableBLACK)
+
+    printPieceSqTable(pawn_tableWHITE)
+    printPieceSqTable(pawn_tableBLACK)
+
     app = QApplication([])
     AIstart = False
     if sys.argv[1] == "white":
@@ -317,3 +487,7 @@ if __name__ == "__main__":
     # window.AIsTurn= sys.argv[1]
     window.show()
     app.exec()
+
+    # # chessboard = chess.Board(fen="1P6/3P4/8/3n4/8/8/3p4/8 w - - 0 1")
+    # chessboard = chess.Board()
+    # print(evaluationFunction(chessboard))
